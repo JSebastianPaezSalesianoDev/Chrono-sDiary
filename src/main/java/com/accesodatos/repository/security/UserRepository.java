044c3586562e044f4429922c094261c915b691d0
Juan Diego Paez Delgado <juandipademanchaz@gmail.com>
2025-05-01 21:27:10 +0100

    feat: creacion de repositorios para usuarios y roles

---
+++ src/main/java/com/accesodatos/repository/security/UserRepository.java
@@ -0,0 +1,14 @@
+package com.accesodatos.repository.security;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.accesodatos.entity.UserEntity;
+
+@Repository
+public interface UserRepository  extends JpaRepository<UserEntity, Long>{
+
+	Optional<UserEntity> findUserEntityByUsername(String name);
+}
