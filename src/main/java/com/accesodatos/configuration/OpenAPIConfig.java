e5f5b87ebe2ad3adeed99c685da7652136487c80
JSebastianPaezSalesianoDev <159430211+JSebastianPaezSalesianoDev@users.noreply.github.com>
2025-05-23 21:11:59 +0100

    Feat: Se agrego terminos y licencias del config api swagger

--- src/main/java/com/accesodatos/configuration/OpenAPIConfig.java
+++ src/main/java/com/accesodatos/configuration/OpenAPIConfig.java
@@ -8,17 +8,17 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
 		info = @Info(
-				title = "P1U4",
-				description = "Our app provides a consice listing of banks,cities and customers",
+				title = "Chrono's Dairy",
+				description = "Our app provides a Dairy to control our time and events",
 				termsOfService = "www.accesodatos/terminos_y_condiciones",
 				version = "1.0.0",
 				contact = @Contact(
-						name = "Diego Paez",
+						name = "Diego Paez - Sebastian Paez",
 						url = "https://aulasalesianoslacuesta.com/",
-						email = "usr3859@salesianos-lacuesta.net "
+						email = "usr3859@salesianos-lacuesta.net - usr3766@salesianos-lacuesta.net"
 				),
 				license = @License(
-						name = "Standard Software Use License for Acceso a Datos",
+						name = "Standard Software Use License for DAM salesinaos la cuesta",
 						url = "http://lacuesta.salesianos.edu/licence"
 				)
 		),
